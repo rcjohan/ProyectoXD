eced1483bd4629b1aeb1078ddb6619da62f78d41
Miles morales <141095026+rcjohan@users.noreply.github.com>
2025-06-18 16:34:29 -0400

    xd

--- pages/AfiliadoPage.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-
-export class AfiliadoPage {
-  constructor(private page: Page) {}
-
-  async ingresarNumeroAfiliado(numero: string) {
-    await this.page.getByRole('textbox', { name: 'Número de Afiliado' }).fill(numero);
-  }
-}
